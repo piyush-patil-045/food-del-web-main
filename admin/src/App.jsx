83738bfc31b8f5a3640a555c3bc3190b998beae5
21bca158 <piyushpatil0126@gmail.com>
2024-10-05 15:21:01 +0530

    Refactor Sidebar component import in App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,7 +3,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Navbar from "./components/Navbar/Navbar";
-import Sidebar from "./components/Sidebar/sidebar";
+import Sidebar from "./components/Sidebar/Sidebar";
 import Add from "./pages/Add/Add";
 import List from "./pages/List/List";
 import Orders from "./pages/Orders/Orders";
